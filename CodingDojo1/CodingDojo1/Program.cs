e895fee566b3f5c0346b3cd5e09d497356f50dab
wi15b149 <wi15b149@technikum-wien.at>
2016-09-27 11:47:08 +0200

    Start

---
+++ CodingDojo1/CodingDojo1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingDojo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tes");
+        }
+    }
+}
