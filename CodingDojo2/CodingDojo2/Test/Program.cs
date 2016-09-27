3315777d5f0e2bda8d0f48d64ba4f6eba98f0834
wi15b149 <wi15b149@technikum-wien.at>
2016-09-27 17:15:06 +0200

    Tests hinzugefügt

--- CodingDojo2/CodingDojo2/Test/Program.cs
+++ CodingDojo2/CodingDojo2/Test/Program.cs
@@ -11,7 +11,68 @@ namespace CodingDojo2.Test
     {
         static void Main(string[] args)
         {
-                        
+            TestWithInt();
+            TestWithString();
+            TestWithObject();
+            Console.ReadLine();
+        }
+        static void TestWithInt()
+        {
+            Stack<int> test = new Stack<int>();
+            Console.WriteLine("Test mit Int");
+            test.Push(5);
+            Console.WriteLine("push: {0}", test.Peek());
+            test.Push(7);
+            Console.WriteLine("push: {0}", test.Peek());
+            test.Push(4);
+            Console.WriteLine("push: {0}", test.Peek());
+            Console.WriteLine("pop: {0}", test.Pop());
+            Console.WriteLine("pop: {0}", test.Pop());
+            Console.WriteLine("last: {0}", test.Peek());
+            Console.WriteLine("pop: {0}", test.Pop());
+            Console.WriteLine("last: {0}", test.Peek());
+            
+        }
+
+        static void TestWithString()
+        {
+            Stack<string> test = new Stack<string>();
+
+            Console.WriteLine("Test mit String");
+            test.Push("s5");
+            Console.WriteLine("push: {0}", test.Peek());
+            test.Push("s7");
+            Console.WriteLine("push: {0}", test.Peek());
+            Console.WriteLine("pop: {0}", test.Pop());
+            test.Push("s4");
+            Console.WriteLine("push: {0}", test.Peek());
+                        Console.WriteLine("pop: {0}", test.Pop());
+            Console.WriteLine("last: {0}", test.Peek());
+            Console.WriteLine("pop: {0}", test.Pop());
+            Console.WriteLine("last: {0}", test.Peek());
+
+        }
+
+        static void TestWithObject()
+        {
+            Stack<TestObject> test = new Stack<TestObject>();
+            Console.WriteLine("Test mit Objekt");
+
+            test.Push(new TestObject(41, "Sampleperson1"));
+            Console.WriteLine("read: {0}", test.Peek());
+            test.Push(new TestObject(80, "Sampleperson2"));
+            Console.WriteLine("read: {0}", test.Peek());
+            test.Push(new TestObject(15, "Sampleperson3"));
+            Console.WriteLine("read: {0}", test.Peek());
+            Console.WriteLine("{0} removed", test.Pop());
+            Console.WriteLine("read: {0}", test.Peek());
+            Console.WriteLine("{0} removed", test.Pop());
+            Console.WriteLine("read: {0}", test.Peek());
+            Console.WriteLine("{0} removed", test.Pop());
+            Console.WriteLine("read: {0}", test.Peek());
+
         }
     }
+
+
 }
---
+++ CodingDojo2/CodingDojo2/Test/TestObject.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingDojo2.Test
+{
+    class TestObject
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+
+        public TestObject(int age, string name)
+        {
+            this.Age = age;
+            this.Name = name;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0} : {1}", Name, Age);
+        }
+    }
+}
